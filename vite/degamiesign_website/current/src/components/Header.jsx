fd5b3e644338c8b85c435850f2a3961ab0ca3ecc
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-13 10:41:08 +0530

    Add files via upload

---
+++ vite/degamiesign_website/current/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Header() {
+  return ( <div className ='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'style={{backgroundImage:"url('/Website_header.png'"}}id='Header'></div>//Header Properties Declare
+  )
+}
+
+export default Header
