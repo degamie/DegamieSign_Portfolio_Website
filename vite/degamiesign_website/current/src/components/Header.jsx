8280b732a6ad42ba5c6c862a36a815f27055e455
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-14 09:37:18 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/Header.jsx
+++ vite/degamiesign_website/current/src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function Header() {
-  return ( <div className ='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'style={{backgroundImage:"url('/Website_header.png'"}}id='Header'></div>//Header Properties Declare
+  return ( <div className ='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'style={{backgroundImage:"url('/Website_header.png'"}}id='Header'>
+    <Navbar/>
+      <h2>
+        We Empower Innovation!
+      </h2>
+      <a href="">Jobs</a>
+      <a href="">Contact Us</a>   
+    </div>
   )
 }
 
