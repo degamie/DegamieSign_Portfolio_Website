d94125aa8443f0064855728957280dcaaedff947
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-18 10:05:29 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/Navbar.jsx
+++ vite/degamiesign_website/current/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function Navbar() {
-  return (  <div>
+  return ( 
+     <div>
+      <div>
+        <ul>
+          <a href="#header classname='px-4 py-2 rounded-full infline-block">About Us</a>
+        </ul>
+      </div>
     <Navbar></Navbar>
+
+    
     
         Navbar</div>
   )
