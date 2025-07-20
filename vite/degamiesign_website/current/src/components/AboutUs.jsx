70e30632d00b8b2f73573f9723c978be4650e5bb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-20 13:31:22 +0530

    Add files via upload

---
+++ vite/degamiesign_website/current/src/components/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+  <div> AboutUs
+    <h2>Welcome,We Provide High End Business Solutions Globally </h2>
+    <div>
+       <h2> Mobile App Dev
+        <div>Design,Build and Develop Most PowerFull App Dev Integrably   </div></h2>
+        {/* TBC */}
+    </div>
+    </div>
+  )
+}
+
+export default AboutUs
