26ea0105fcc16568f60230a019d2c69e5c050f7f
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-22 23:28:25 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/AboutUs.jsx
+++ vite/degamiesign_website/current/src/components/AboutUs.jsx
@@ -4,7 +4,8 @@ function AboutUs() {
   return (
   <div> AboutUs
     <h2>Welcome,We Provide High End Business Solutions Globally </h2>
-    <div>
+    <div className='flex flex-col items-center container mx-auto p-14 md:px-20 lg:px-35 w-full overflow-display 'id='About Us'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'></h1>
        <h2> Mobile App Dev
         <div>Design,Build and Develop Most PowerFull App Dev Integrably   </div></h2>
         {/* TBC */}
