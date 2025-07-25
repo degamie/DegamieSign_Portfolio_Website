9a9662ded2f5cfea656f5349b0d3aeb1f3be60ff
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-25 09:12:40 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/AboutUs.jsx
+++ vite/degamiesign_website/current/src/components/AboutUs.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 function AboutUs() {
-  return (
+  return ( 
   <div> AboutUs
     <h2>Welcome,We Provide High End Business Solutions Globally </h2>
     <div className='flex flex-col items-center container mx-auto p-14 md:px-20 lg:px-35 w-full overflow-display 'id='About Us'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'></h1>
        <h2> Mobile App Dev
-        <div>Design,Build and Develop Most PowerFull App Dev Integrably   </div></h2>
+        <div>Design,Build and Develop Most PowerFull App Dev Integrably</div></h2>
+        <div className='grid grid-cols-2 gap-8 md:gap-4 w-full 2xl:pr-28'>
+        <p className ='text-5xl font-medium text-blue-white-800'>Believe In!</p>
+        <p className ='text-5xl font-medium text-yellow-white-800'>Hiring</p>
+        </div>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'></div>
         <img src={assets}alt=""></img>
     </div>
