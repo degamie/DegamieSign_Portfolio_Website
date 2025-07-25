c2addc8cf38056a38e3bf0e102908c92f804a18d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-25 18:20:41 +0530

    Add files via upload

---
+++ Jobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Jobs() {
+  return (
+  <div>Jobs
+    <div className='Container mx-auto px-8 py-6 pt-10 md:px-15 py-6 pt-10 px-8 md:px-15 px-34 my-25 w-full overflow:hidden'></div>
+</div>
+  )
+}
+
+export default Jobs
