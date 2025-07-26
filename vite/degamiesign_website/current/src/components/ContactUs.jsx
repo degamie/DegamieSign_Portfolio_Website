f319f18107e68c4283633d165b24a16f557c3492
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-26 15:49:00 +0530

    Add files via upload

---
+++ vite/degamiesign_website/current/src/components/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div className='Container mx-auto py-4 pt-16 px-7 md:px-30 lg:px-32 my-20 w-full overflow-hidden'id='Contact Us'>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>ContactUs </h1>
+        <p className='text-center text-purple-500  mb-8 max-w-80 mx-auto'></p>
+        <p className='text-center text-orange-500  mb-8 max-w-85 mx-auto'>Reach Us!</p>
+    </div>
+  )
+}
+
+export default ContactUs
