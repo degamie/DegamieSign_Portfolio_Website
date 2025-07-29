af20cd724f1cd671f484165225ff23db5dfbaaf9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-30 00:45:33 +0530

    Add files via upload

---
+++ vite/degamiesign_website/current/src/assets/assets.js
@@ -0,0 +1,14 @@
+export const ProductData{
+    title:"Mobile Application's Developmnet";
+    Body:"We Design,Build Develop the Most Powerful Applications";
+    image:'assets.{mobile_app.png}',
+}
+{
+    title:'Futuristic Connection Devices',
+    Body:'Develop The Hardware Highly Connective in Single Instances at Large Distances Also',
+    image:'assets.{Connection_Devices.png}'.
+}
+    title:"Engaging People's Integrable Ideas";
+    Body:"In Solving People Integratbale :Sharable Ideas People Growth In Market Pracically";
+    image:'assets.{Integrable_Ideas.png}'
+}
