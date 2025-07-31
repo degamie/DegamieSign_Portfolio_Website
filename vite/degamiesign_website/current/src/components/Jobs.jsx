6e8695a9567cc5eafd871e7fb809faaa528d3a5c
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-31 08:56:48 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/Jobs.jsx
+++ vite/degamiesign_website/current/src/components/Jobs.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Jobs() {
+  const [SetCurrentIndx,SetCurrentxJob]=useState(false);
+  const nextJob=()=>{
+    SetCurrentIndx((prevIndx)=>(prevIndx+1)%JobsData.length);
+  }
+ const PrevJob=()=>{
+    SetCurrentIndx((prevIndx)=>(prevIndx===0)?JobsData.length-1:PrevIndx-1);
+  }
+
   return (
   <div>Jobs
     <div className='Container mx-auto px-8 py-6 pt-10 md:px-15 py-6 pt-10 px-8 md:px-15 px-34 my-25 w-full overflow:hidden'></div>
