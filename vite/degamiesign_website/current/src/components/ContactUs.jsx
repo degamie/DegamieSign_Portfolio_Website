9ceb8182e9e52e6c668c70928ac0565ebde807a5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-02 18:26:36 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/ContactUs.jsx
+++ vite/degamiesign_website/current/src/components/ContactUs.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 function ContactUs() {
   return (
+    <div>
+     <div className='w-full md:w-1/2 text-left'>Name 
+      <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="Name" placeholder='Your FullName Required'>
+      </input>
+            <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="Email" placeholder='Your Email Required'></input>
+                  <input className='w-full border border-blue-350 rounded py-5 px-4 mt-1'type="text" name="PhoneNumber" placeholder='Your PhoneNumber Required'></input>
+
+     </div>
+    
+  
     <div className='Container mx-auto py-4 pt-16 px-7 md:px-30 lg:px-32 my-20 w-full overflow-hidden'id='Contact Us'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>ContactUs </h1>
         <p className='text-center text-purple-500  mb-8 max-w-80 mx-auto'></p>
         <p className='text-center text-orange-500  mb-8 max-w-85 mx-auto'>Reach Us!</p>
-    </div>
+  
+     </div> </div>
   )
 }
 
