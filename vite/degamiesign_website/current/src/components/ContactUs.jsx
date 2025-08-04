c08b696f71b211b1745fe9e5f5175e9ed28d43ca
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-04 08:17:00 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/ContactUs.jsx
+++ vite/degamiesign_website/current/src/components/ContactUs.jsx
@@ -14,12 +14,17 @@ function ContactUs() {
 
     </form>
   
+        <div className='my-6 text-left'>
+          <button className='bg-blue-650 text-purple py-2 px-12 py-2 px-12 py-5 mb-10 rounded'>Send Message</button>
     <div className='Container mx-auto py-4 pt-16 px-7 md:px-30 lg:px-32 my-20 w-full overflow-hidden'id='Contact Us'>
         <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>ContactUs </h1>
         <p className='text-center text-purple-500  mb-8 max-w-80 mx-auto'></p>
         <p className='text-center text-orange-500  mb-8 max-w-85 mx-auto'>Reach Us!</p>
-  
+ 
+
+       </div>
      </div> </div>
+
   )
 }
 
