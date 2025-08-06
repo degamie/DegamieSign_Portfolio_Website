643cf487fecb8b6b552d3ac240a72482e2fa22bf
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-07 00:23:40 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/Jobs.jsx
+++ vite/degamiesign_website/current/src/components/Jobs.jsx
@@ -8,12 +8,17 @@ function Jobs() {
  const PrevJob=()=>{
     SetCurrentIndx((prevIndx)=>(prevIndx===0)?JobsData.length-1:PrevIndx-1);
   }
+  const nextProduct=()=>{
+    SetCurrentIndx(prevIndx)=>(prevIndx+1)%ProductsData.length();
+  }
 
   return (
   <div>Jobs
     <div className='Container mx-auto px-8 py-6 pt-10 md:px-15 py-6 pt-10 px-8 md:px-15 px-34 my-25 w-full overflow:hidden'></div>
     {/* Buttons Slider Container */}
+      <div className='flex gap-9 transition-duration-500 -ease-in-out style={transform:translate(-50-{SetCurrentIndx*100)}'>Jobs Hiring</div>
     <button className='p-3 bg-blue-200 rounded mr-220' aria label="Join Us Now">
+    
        {/*Right  Arrow */}
       <img src={assets.right_arrow} alt='Now'></img>
     </button>
@@ -21,7 +26,7 @@ function Jobs() {
     <button className='p-3 bg-blue-200 rounded mr-220' aria label="Discover How our Solutions have been Built">
       <img src={assets.left_arrow} alt='Built'></img>
     </button>
-  
+   
 </div>
   )
 }
