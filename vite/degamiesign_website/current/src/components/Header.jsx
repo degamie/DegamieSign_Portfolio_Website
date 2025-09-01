28217fa375316baca8563649717326d1d8953194
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-01 07:10:08 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/Header.jsx
+++ vite/degamiesign_website/current/src/components/Header.jsx
@@ -1,21 +1,6 @@
-import React from 'react'
-import Navbar from './Navbar'
-
-function Header() {
-  return ( <div className ='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'style={{backgroundImage:"url('/Website_header.png'"}}id='Header'>
-    <Navbar/>
-      <h2>
-        We Empower Innovation!
-      </h2>
-      <div className="grid 2">
-        <div>
-          <button className='md-85 blue-color px-5 py-6'>Header</button>
-        </div>
-      </div>
-      <a href="">Jobs</a>
-      <a href="">Contact Us</a>   
-    </div>
-  )
-}
-
-export default Header
+<div>
+    <motion.div
+        initial={{opacity:0,y:100}}
+        transition={{duration:1.53}}
+        whileInView={{opacity:0,y:1.0}}
+        ></motion.div></div>
