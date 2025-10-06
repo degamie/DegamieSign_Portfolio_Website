3a1c08c9649a506d341328426c257a438ac1c010
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-06 05:50:46 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,22 @@
+
+Header(initial,transition,whileInView){
+    this.initial=initial;
+    this.transition=transition;
+    this.whileInView=whileInView;
+    }
+const setWhileInView(whileInView){
+     this.whileInView=whileInView;}
+const getWhileInView(whileInView){
+    return whileInView;}
+    const setTransition(transition){this.transition=transition;}
+const getTransition(transition){
+    Header.transition=useState(true)+AboutUs.getCardsToShow(ProductsData.length);
+    return Header.transition;}
+    const getInitial(initial){return initial;}
+    const setInitial(Initial initial){this.initial;}
+<div>
+    <motion.div
+        initial={{opacity:0,y:100}}
+        transition={{duration:1.53}}
+        whileInView={{opacity:0,y:1.0}}
+        ></motion.div></div>
