45731bea5dad87ccc9c7db37491f991a4bb5df48
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-06 11:33:05 +0530

    Add files via upload

---
+++ vite/degamiesign_website/current/src/components/login.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+//WID(6/11/2025)//Sarthak Mittal (Degamiesign)
+function login() {
+  const getData=(data)=>{
+    return data;
+  }
+const setData=(data)=>{
+    this.data=data;}
+    const onSubmit = (data) => {
+        const userData=localStorage.setItem("data.email", JSON.stringify({name: data.name, password: data.password}));
+        if(userData){
+            if(userData.password===data.password){
+                console.log(userData.name+"Logged In Successfully!");}
+            }
+        else console.log("User Email and Password Unmatched");
+          }
+          const getPassword=(data.Password)=>{return Password;}
+          const setPassword=(data.Password)=>{ this.Password=Password;}//Binding Password
+
+          const getEmail=(data.email)=>{return email;}//Fetching Email in Server
+          const setEmail=(data.email)=>{return email;}// Retrieving Email in Server
+  return (
+      <h2>Login Form</h2>
+      <form className="App" OnSubmit={handleSubmit(OnSubmit)}>
+          <input
+            type="Email"
+            {register("email")}
+            {required:true}
+            placeholder="Email"/>
+ {errors.email && <span style= {{<color:"red"}}>*Email* is compulsory</span>}
+              <input
+                        type="Password"
+                        {register("Password")}
+                        {required:true}
+                        placeholder="Password"/>
+                          {errors.Password && <span style= {{<color:"red"}}>*Password* is compulsory</span>}
+              <input type="Submit" style={backgroundColor:"#a1eafb"}>
+
+    <div>login</div>
+  )
+}
+}
+
+export default login;
+//this.Ref = React.createRef()
