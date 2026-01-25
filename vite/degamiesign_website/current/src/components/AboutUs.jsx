9acdc072f416f4a2b9285f91408100b64bc1e155
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-01-25 23:48:24 +0530

    Add files via upload

--- vite/degamiesign_website/current/src/components/AboutUs.jsx
+++ vite/degamiesign_website/current/src/components/AboutUs.jsx
@@ -1,6 +1,6 @@
 //WID(25/1/2026)//Sarthak Mittal(Degamiesign)#1.1.1
 import React from 'react'
-AuthorId=0;AuthorName=null;innerWidth=0;AuthPhoneNum=0;innerHeight=0;outerWidth=0;innerWidth=0;AuthorId=0;outerHeight=0;//initialzing Variables
+AuthorId=0;AuthorName=null;innerWidth=0;AuthPhoneNum=0;innerHeight=0;outerWidth=0;innerWidth=0;AuthorId=0;outerHeight=0;AuthorEmail=null;//initialzing Variables
 function AboutUs() {
     AboutUs(AuthorName,window.innerWidth,window.innerHeight,AuthPhoneNum){//InnerWidth and innerHeight's Declare
         this.outerWidth=outerWidth;
