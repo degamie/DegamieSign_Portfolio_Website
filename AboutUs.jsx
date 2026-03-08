c38fcb822bb31a55ce9b479631c5234a1271f6e8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-08 11:41:57 +0530

    Add files via upload

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,99 +1,61 @@
-//WID(10/1/2026)//Sarthak Mittal(Degamiesign)#1
+//WIP(08/03/2026)(Sarthak Mittal)#1.1.1.1.1c.1cC.1cC.1.1.1.1c   
 import React from 'react'
-
+Window window=new Window();
+getWinodw=(window)=>{return window;}//Fetching window in App
+Product product=new Product();
+existsByProduct=(product)=>{if(product!=null)getProduct(product);else getProduct(null);}//Chekcing Product's Existence in App
+updateByProduct=(product)=>{getProduct(product)+setProduct(product)+1;}//Updating Product in App
+setProduct(product){this.product=product;}//binding Product
+getProduct(product){return product;}//Fethcing Product in App
+window.outerHeight=0;
+window.outerWidth=0;
+window.innerheight=0;
+Window.innerWidth=0;
 function AboutUs() {
-    AboutUs(AuthorName,window.innerWidth,window.innerHeight,AuthPhoneNum){//InnerWidth and innerHeight's Declare
-        this.AuthorName=AuthorName;
-        this.innerWidth=innerWidth;//Binding InnerWidth
-        this.innerHeight=innerHeight;//InnerWidth and innerHeight's Binding
-        this.AuthPhoneNum=AuthPhoneNum;//AuthPhoneNum Binding
-        }
   return (
-      setinnerWidth(InnerWidth){this.InnerWidth=InnerWidth;}
-      getinnerWidth(InnerWidth){return InnerWidth;}
-      getAuthorId=(AuthorId)={return AuthorId;}//Fetching AuthorId in Server
-      setAuthorId=(AuthorId)={this.AuthorId=AuthorId;}//Binding AuthorId in Servers
-      updateAllByAuthorId=(AuthorId)=>{getAuthorId(AuthorId)+setAuthorId(AuthorId)+1;}//Updating AuthorId in App
-      existsByAuthorId=(AuthorId)=>{if(AuthorId>0)getAuthorId(AuthorId);else getAuthorId(0);}//Checking AuthorId's Existence in App
-       updateByinnerWidth=(InnerWidth)=>{getinnerWidth(innerWidth)+setinnerWidth(innerWidth)+1;}//Updating Inner Width in It
-       updateByinnerHeight=(InnerHeight)=>{getinnerHeight(innerHeight)+setinnerHeight(innerHeight)1;}//updating Inner height inn App
-      setAuthorName(AuthorName){this.AuthorName=AuthorName;}
-      getAuthorName(AuthorName){return AuthorName;}
-      updateAllByAuthorName=(AuthorName)=>{getAuthorName(AuthorName)+setAuthorName(AuthorName)+1;}//Udpating All Author's Name in Server
-      setProductsDataToShow(ProductData.length){this.ProductData=ProductData;}
-    getProductsToShow=(ProductsData.length)=>{
-      return ProductsData+1;
-    }
-getPhoneNum=(AuthPhoneNum)=>{return AuthPhoneNum;}//Fethcing AuthPhoneNum in App
-setPhoneNum=(AuthPhoneNum)=>{this.AuthPhoneNum=AuthPhoneNum;}//Binding AuthPhoneNum in App
-updateAllByProductsToShow=(ProductData.length)=>{
-    getProductsToShow(ProductData.length)+setProductsDataToShow(ProductData.length)+1;
-    }
-const existsByAuthorName=(authorName)={if(authorName!==null)getAuthorName(authorName);else getAuthorName(authorName);}//Checking AuthorName in App
-
-setOuterWidth(outerWidth.length){this.outerWidth=outerWidth;}
-getOuterHeight(outerHeight.length){return outerHeight;}//Fetching OuterHeight in Server
-setOuterHeight(outerHeight.length){this.outerHeight=outerHeight;}
-updateByOuterHeight=(outerHeight.length){getOuterHeight(outerHeight)+setOuterHeight(outerHeight)+1;}//Updating OuterHieght in App
-const existsByOuterWidth=(OuterWidth)=>{if(outerWidth>0)getOuterWidth(outerWidth);else getOuterWidth(0);}//Checking OuterWidth's Existence in APpp
-getOuterWidth(outerWidth.length){
-    return outerWidth;}
-    setOuterWidth=(outerWidth.length){this.outerWidth=outerWidth;}//Binding OuterWidth in Server
-    const updateByouterWidth=(outerWidth)=>{getOuterWidth(outerWidth)+setOuterWidth(outerWidth)+1;}//Updating Outer Width in Server
-      setCardsToShow(ProductsData.length){
-          this.ProductsData.length=ProductsData.length;}
-          getCardSToShow=(ProductData.length)=>{return ProductsData.length; }//Fetching ProductData in Server
-                 getinnerWidth(window.innerWidth){
-                                   return innerWidth+1;}
-                                   setinnerWidth(window.innerWidth){
-                                       this.innerWidth=innerWidth;  }
-                                   const updateCardsToShow=(ProductData.length)=>{getCardSToShow(ProductData)+setCardsToShow(ProductData+1;}//Updating Cards To Show
      <div>
       <div>
-          getinnerHeight(window.innerHeight){
-              return innerHeight;}
-
           const useEffect=(()=>{
           const updateCardsToShow=()=>{
             if(window.innerWidth>=1024){
               setCardsToShow(ProductsData.length);
             }
               else setCardsToShow(1);
-              window.addEventListener();   
+              window.addEventListener();
           }
 
         });
     return ()=>{
       window.removeEventListner('resize',updateCardsToShow);
        window.addEventListener("resize",updateCardsToToShow());
-    }       
+    }
   <div> AboutUs
     <h2>Welcome,We Provide High End Business Solutions Globally </h2>
     <div className='flex flex-col items-center container mx-auto p-14 md:px-20 lg:px-35 w-full overflow-display 'id='About Us'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'></h1>
               <p className ='text-5xl font-medium text-blue-white-800'>Believe In!</p>
               <div className='flex gap-8 transition-transform duration-500 ease-in-out 'style={{transform:'translateX'('$(currentIndx*100/cardsTodisplaty'%)}}
-             
-//         <div>
-//           <div>
-//             {ProductData.map((Product,index)=>(
-//               <div key={index} className='relative flex -shrink-0 w-full sm:w-1/4'>
-//                 <img src={ProductImg} alt={Product.img}>
-//                 </img>
-//                 </div>
-//             ))}
-//           </div>
-//         </div>
-//        <h2> Mobile App Dev
-//         <div>Design,Build and Develop Most PowerFull App Dev Integrably</div></h2>
-//         <div className='grid grid-cols-2 gap-8 md:gap-4 w-full 2xl:pr-28'>
+
+        <div>
+          <div>
+            {ProductData.map((Product,index)=>(
+              <div key={index} className='relative flex -shrink-0 w-full sm:w-1/4'>
+                <img src={ProductImg} alt={Product.img}>
+                </img>
+                </div>
+            ))}
+          </div>
+        </div>
+       <h2> Mobile App Dev
+        <div>Design,Build and Develop Most PowerFull App Dev Integrably</div></h2>
+        <div className='grid grid-cols-2 gap-8 md:gap-4 w-full 2xl:pr-28'>
 
         <p className ='text-5xl font-medium text-yellow-white-800'>Hiring</p>
         </div>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'></div>
         <img src={assets}alt=""></img>
     </div>
-  
+
       </div>
     </div>
 
