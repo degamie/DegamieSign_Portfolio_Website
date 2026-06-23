e6b81fe77e13265b0c04c14fc894bb3e4d0f1946
Degamie_sign <degamiesign@gmail.com>
2026-06-23 08:30:11 +0530

    PhoneNum's Binding hook#1

--- DegamieSign_Portfolio_Website/src/Contact.jsx
+++ DegamieSign_Portfolio_Website/src/Contact.jsx
@@ -1,7 +1,8 @@
-//WID(8/6/2026)(SarthaK Mittal(DegmaieSign)(ContactUs))
-import React from 'react'
+//WID(23/6/2026)(SarthaK Mittal(DegmaieSign)(ContactUs))
+import React, { useState } from 'react'
 
 export default  function Contact() {
+  const [phoneNum,setphoneNum]=useState(true);
   return (
     <section id="contact" style={{textAlign:"center",display:'flex',flexdirection:'column',justifyContent:'center', alignItems:'center'}}>
     <div>Contact Us!</div>
